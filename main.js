59d2e5913b4468ca63f73a08793efff7d41e0575
moaaz-khaled <mo3az.khalid.2003@gmail.com>
2025-08-17 20:43:22 +0300

    Add Skill Section

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-
-
 let Menu = document.querySelector(".menu-toggle");
 let List = document.querySelector(".navbar ul");
 
 Menu.addEventListener("click" , () =>{
     List.classList.toggle('active');
 })
+
+const skillBars = document.querySelectorAll('.color');
+
+skillBars.forEach(bar => {
+    const percentageText = bar.closest('.lang-container , .personal-skill').querySelector('.percentage').textContent;
+    const percentage = parseInt(percentageText, 10);
+    bar.style.setProperty('--dynamic-width', `${percentage}%`);
+});
